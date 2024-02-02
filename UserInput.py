69bda09174a9484d95907a84ad942fb5080db21b
nadiralam <147323201+nadiralam@users.noreply.github.com>
2024-02-02 20:06:39 +0530

    Add files via upload

---
+++ UserInput.py
@@ -0,0 +1,14 @@
+a = input("enter the number:- ")
+print(a)
+
+b = input("Enter the country name whrere am i living now:- ")
+print("my country name is:-",b)
+
+
+c = input("Enter the number:- ")
+C = int(c)
+d = input("Enter the number:- ")
+D = int(d)
+
+add = C+D
+print(add)
