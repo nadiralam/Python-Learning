88b44a1f7616fa111dbe33d6b534bc8c729863e3
nadiralam <147323201+nadiralam@users.noreply.github.com>
2024-02-02 19:53:23 +0530

    Add files via upload

---
+++ varaible.py
@@ -0,0 +1,57 @@
+rent = 4585
+gas = 346.7
+rice = 646
+print(rice)
+print(gas)
+print(rent)
+
+total  = rent + gas + rice
+print(total) 
+ 
+avg = total/3
+print(avg)
+
+item1 = "rent"
+item2 = "gas"
+item3 = "rice"
+print("Expense item:-" ,item1,item2,item3)
+
+# camel case of variable
+myNameIs = "Nadir"
+
+#pascal case of varaible
+MyNameIs = "john"
+
+#Snake case of variable
+my_name_is = "ali"
+
+
+#python follows Dynamic typing which means it's doesn't declare the variable and it automatically identifies the type of variable.
+
+# Python follows Dynamic binding which means one variable store multiple data type value.
+
+name = "nadir"
+print(name)
+
+name = 34
+print(name)
+
+name = 5435.7
+print(name)
+
+
+# special syntax
+a=4;b=5;c=7
+print(a)
+print(b)
+print(c)
+
+a,b,c = 2,3,4
+print(a)
+print(b)
+print(c)
+
+a=b=c=9
+print(a)
+print(b)
+print(c)
