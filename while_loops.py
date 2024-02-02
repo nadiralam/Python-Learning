3fc411fd7cf157ad9953687700a3f9a92f7d3ea7
nadiralam <147323201+nadiralam@users.noreply.github.com>
2024-02-02 20:13:34 +0530

    Add files via upload

---
+++ while_loops.py
@@ -0,0 +1,14 @@
+# number = int(input("Enter the number:- "))
+# i = 1
+# while i<11:
+#     print(number * i)
+#     i+=1
+# print("code is successfully done!")
+
+
+number = int(input("Enter the number:- "))
+i = 1
+while i<=10:
+    print(number,"*",i,"=",number * i)
+    i+=1
+print("code is succesfully done!")  
